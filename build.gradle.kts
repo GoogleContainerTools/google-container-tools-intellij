31a7231c57b5bf22d4c972b46cd68e025e9c6ca0
Ivan Portyankin <ivan.portyankin@gmail.com>
2018-09-06 16:39:56 -0400

    Enables Kotlin support with Gradle plugin for compilation and runtime.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,14 @@
+repositories {
+    jcenter()
+}
+
 plugins {
     id("org.jetbrains.intellij") version "0.3.6"
+
+    kotlin("jvm") version "1.2.61"
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+    compile(kotlin("runtime"))
 }
