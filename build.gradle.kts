001afca1d1edf3f5172ffd8bff567fbec07412bb
Ivan Portyankin <ivan.portyankin@gmail.com>
2018-09-17 16:41:50 -0400

    apply plugins and repositories for all plugins including the main module.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,10 @@
-repositories {
-    jcenter()
-}
-
 plugins {
     id("org.jetbrains.intellij") version "0.3.7"
 
     kotlin("jvm") version "1.2.61"
 }
 
-subprojects {
+allprojects {
     repositories {
         jcenter()
     }
