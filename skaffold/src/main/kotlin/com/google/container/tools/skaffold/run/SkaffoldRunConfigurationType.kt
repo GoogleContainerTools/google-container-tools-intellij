311183db85dc6eb67ccd90117b1bec65cd852d3e
Ivan Portyankin <ivan.portyankin@gmail.com>
2018-09-18 15:55:11 -0400

    Implements basic methods of ConfigurationType, adds UI resources via internal functions and icons.

--- skaffold/src/main/kotlin/com/google/container/tools/skaffold/run/SkaffoldRunConfigurationType.kt
+++ skaffold/src/main/kotlin/com/google/container/tools/skaffold/run/SkaffoldRunConfigurationType.kt
@@ -1,19 +1,40 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.container.tools.skaffold.run
 
+import com.google.container.tools.skaffold.SKAFFOLD_ICON
+import com.google.container.tools.skaffold.message
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.ConfigurationType
-import com.intellij.icons.AllIcons
 import javax.swing.Icon
 
-open class SkaffoldRunConfigurationType : ConfigurationType {
+/**
+ * [ConfigurationType] extension that registers and enables Skaffold run configuration type, including single run and
+ * continuous deployment targets provided by separate [ConfigurationFactory].
+ */
+class SkaffoldRunConfigurationType : ConfigurationType {
     val ID = "google-container-tools-skaffold-run-config"
 
     override fun getIcon(): Icon {
-        return AllIcons.General.Balloon
+        return SKAFFOLD_ICON
     }
 
     override fun getConfigurationTypeDescription(): String {
-        return "Skaffold"
+        return message("skaffold.run.config.general.description")
     }
 
     override fun getId(): String {
@@ -21,7 +42,7 @@ open class SkaffoldRunConfigurationType : ConfigurationType {
     }
 
     override fun getDisplayName(): String {
-        return "Skaffold"
+        return message("skaffold.run.config.general.name")
     }
 
     override fun getConfigurationFactories(): Array<ConfigurationFactory> {
