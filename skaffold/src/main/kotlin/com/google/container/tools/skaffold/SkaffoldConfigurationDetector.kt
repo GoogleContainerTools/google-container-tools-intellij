971ab4ae466abcd1ea1edee23ec496f134674b9a
Ivan Portyankin <ivan.portyankin@gmail.com>
2018-12-04 15:36:19 -0500

    expanding on implementation, notification for user to select which configuration to create or skip.

---
+++ skaffold/src/main/kotlin/com/google/container/tools/skaffold/SkaffoldConfigurationDetector.kt
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2018 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.container.tools.skaffold
+
+import com.google.container.tools.core.PLUGIN_NOTIFICATION_DISPLAY_GROUP_ID
+import com.google.container.tools.skaffold.run.AbstractSkaffoldRunConfiguration
+import com.google.container.tools.skaffold.run.SkaffoldDevConfiguration
+import com.google.container.tools.skaffold.run.SkaffoldDevConfigurationFactory
+import com.google.container.tools.skaffold.run.SkaffoldRunConfigurationType
+import com.google.container.tools.skaffold.run.SkaffoldSingleRunConfiguration
+import com.google.container.tools.skaffold.run.SkaffoldSingleRunConfigurationFactory
+import com.intellij.execution.RunManager
+import com.intellij.execution.configurations.RunConfiguration
+import com.intellij.execution.impl.RunManagerImpl
+import com.intellij.execution.impl.RunnerAndConfigurationSettingsImpl
+import com.intellij.notification.NotificationDisplayType
+import com.intellij.notification.NotificationGroup
+import com.intellij.notification.NotificationType
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.ProjectComponent
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+
+/**
+ * Detects if project has Skaffold configuration but no Skaffold run targets configured. Uses
+ * [ProjectComponent] to watch project opening, and prompts to create dev/deploy configurations
+ * if they don't exist yet.
+ */
+class SkaffoldConfigurationDetector(val project: Project) : ProjectComponent {
+    private val NOTIFICATION_GROUP = NotificationGroup(
+        PLUGIN_NOTIFICATION_DISPLAY_GROUP_ID,
+        NotificationDisplayType.BALLOON,
+        true,
+        null,
+        SKAFFOLD_ICON
+    )
+
+    override fun projectOpened() {
+        val skaffoldFiles = SkaffoldFileService.instance.findSkaffoldFiles(project)
+        println(skaffoldFiles)
+        if (skaffoldFiles.isNotEmpty()) {
+            val allRunConfigurations = RunManager.getInstance(project).allConfigurationsList
+            println("allConfigs: $allRunConfigurations")
+            val skaffoldRunConfigList =
+                allRunConfigurations.filter { it is AbstractSkaffoldRunConfiguration }
+            if (skaffoldRunConfigList.isEmpty()) {
+                // existing Skaffold config files, but no skaffold configurations, prompt
+                showPromptForSkaffoldConfigurations(project, skaffoldFiles[0])
+            }
+        }
+    }
+
+    private fun showPromptForSkaffoldConfigurations(
+        project: Project,
+        skaffoldFile: VirtualFile
+    ) {
+        val notification = NOTIFICATION_GROUP.createNotification(
+            "Skaffold Configuration Detected", null,
+            "Skaffold configuration file(s) detected, would you like to create run configurations for it?",
+            NotificationType.INFORMATION
+        )
+
+        notification.addAction(object :
+            AnAction("Create both development and deploy configurations") {
+            override fun actionPerformed(e: AnActionEvent?) {
+                addSkaffoldRunConfiguration(skaffoldFile.path)
+                addSkaffoldDevConfiguration(skaffoldFile.path)
+                notification.expire()
+            }
+        })
+
+        notification.addAction(object : AnAction("Create development on Kubernetes configuration") {
+            override fun actionPerformed(e: AnActionEvent?) {
+                addSkaffoldDevConfiguration(skaffoldFile.path)
+                notification.expire()
+            }
+        })
+
+        notification.addAction(object :
+            AnAction("Create single deploy to Kubernetes configuration") {
+            override fun actionPerformed(e: AnActionEvent?) {
+                addSkaffoldRunConfiguration(skaffoldFile.path)
+                notification.expire()
+            }
+        })
+
+
+        notification.addAction(object : AnAction("Skip") {
+            override fun actionPerformed(e: AnActionEvent?) {
+                notification.expire()
+            }
+        })
+
+        notification.notify(project)
+    }
+
+    private fun addSkaffoldDevConfiguration(skaffoldFilePath: String) {
+        val skaffoldDevSettings = SkaffoldDevConfiguration(
+            project,
+            SkaffoldDevConfigurationFactory(SkaffoldRunConfigurationType()),
+            "development on Kubernetes"
+        )
+        skaffoldDevSettings.skaffoldConfigurationFilePath = skaffoldFilePath
+
+        createRunConfigurationFromSettings(skaffoldDevSettings)
+        println("added dev config for skaffold")
+    }
+
+    private fun addSkaffoldRunConfiguration(skaffoldFilePath: String) {
+        val skaffoldRunSettings = SkaffoldSingleRunConfiguration(
+            project,
+            SkaffoldSingleRunConfigurationFactory(SkaffoldRunConfigurationType()),
+            "deploy to Kubernetes"
+        )
+        skaffoldRunSettings.skaffoldConfigurationFilePath = skaffoldFilePath
+
+        createRunConfigurationFromSettings(skaffoldRunSettings)
+        println("added run config for skaffold")
+    }
+
+    private fun createRunConfigurationFromSettings(runConfiguration: RunConfiguration) {
+        val runnerAndConfigSettings = RunnerAndConfigurationSettingsImpl(
+            RunManagerImpl.getInstanceImpl(project),
+            runConfiguration
+        )
+        RunManager.getInstance(project).addConfiguration(runnerAndConfigSettings)
+        RunManager.getInstance(project).selectedConfiguration = runnerAndConfigSettings
+    }
+}
