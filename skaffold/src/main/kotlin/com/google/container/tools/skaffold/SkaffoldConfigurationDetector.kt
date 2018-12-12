e0c5c902dc0800bae1b3dfd75b9af90b8fe8f72a
Ivan Portyankin <ivan.portyankin@gmail.com>
2018-12-12 10:54:46 -0500

    fix unit tests to work with dumb/smart mode.

--- skaffold/src/main/kotlin/com/google/container/tools/skaffold/SkaffoldConfigurationDetector.kt
+++ skaffold/src/main/kotlin/com/google/container/tools/skaffold/SkaffoldConfigurationDetector.kt
@@ -58,6 +58,8 @@ class SkaffoldConfigurationDetector(val project: Project) : ProjectComponent {
     )
 
     override fun projectOpened() {
+        // on project open there may be a lot of indexing and other preparation and files still
+        // not available, wait for "smart" mode for configuration identification
         DumbService.getInstance(project).runWhenSmart {
             val skaffoldFiles: List<VirtualFile> =
                 SkaffoldFileService.instance.findSkaffoldFiles(project)
