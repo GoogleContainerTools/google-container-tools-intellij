b726a57bf6539e66e87cf9b7f7e4ab9d4bae9c84
Ivan Portyankin <ivan.portyankin@gmail.com>
2019-01-29 11:02:38 -0500

    more IJ oriented hello message.

--- examples/hello-spring-boot/src/main/java/hello/HelloController.java
+++ examples/hello-spring-boot/src/main/java/hello/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
     @RequestMapping("/")
     public String index() {
-        return "Hello, World!";
+        return "Hello, World of Kubernetes with IntelliJ!";
     }
 }
